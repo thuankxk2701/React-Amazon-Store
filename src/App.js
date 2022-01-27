bb164e6137d17b777ab1e7ae78850bc74fd865ef
Thuan <ttctde@gmail.com>
2022-01-27 21:23:59 +0700

    update React Amazon Story

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Checkout from "./components/Checkout/Checkout";
 import Login from "./components/Login/Login";
+import Payment from "./components/Payment/Payment";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import { SET_USER } from "./types";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 import { Routes, Route } from "react-router-dom";
-
+const promise = loadStripe(
+  "pk_test_51KMVvJBQaKWgNKgOY470N4g5UUdNcy8zxHvvOFYYJD9j9KgBl9AVaD7qgiDtgx8iWLyXYDHfzcxBZERtz5OeX9oj00YN07vP7j",
+);
 const App = () => {
   const [{}, dispatch] = useStateValue();
   useEffect(() => {
@@ -54,6 +59,17 @@ const App = () => {
           element={
             <>
               <Login />
+            </>
+          }
+        />
+        <Route
+          path="/payment"
+          element={
+            <>
+              <Header />
+              <Elements stripe={promise}>
+                <Payment />
+              </Elements>
             </>
           }
         />
